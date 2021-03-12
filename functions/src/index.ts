26d0d51d04926c1506fcd63752808b21175c9922
Austin Jackson <themacphage@gmail.com>
2021-03-12 16:17:50 -0600

    explode everything to reassemble

---
+++ functions/src/index.ts
@@ -0,0 +1,16 @@
+import * as functions from "firebase-functions";
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+export * as groupA from './groupA';
+
+// Export examples
+// export { Course, PublicationInfo } from './Course';
+// export { default as User } from './User';
+// export * as GradeDistributionCSVRow from './GradeDistributionCSVRow';
