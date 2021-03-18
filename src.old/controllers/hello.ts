83b27955f95a93f5428a9131946439e45926cf4b
Austin Jackson <themacphage@gmail.com>
2021-03-18 04:13:35 -0500

    out with the old~

--- src.old/controllers/hello.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-/**
- * GET /hello
- * Home page.
- */
-export async function world(req: Request, res: Response<string>) {
-  return res.json('Hello, World!');
-}
