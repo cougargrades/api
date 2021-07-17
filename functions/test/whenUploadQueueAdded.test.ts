8b880e3ca5ac9ee727a9a161db088f236f5e428f
Austin Jackson <themacphage@gmail.com>
2021-07-17 14:50:25 -0500

    removed comments

--- functions/test/whenUploadQueueAdded.test.ts
+++ functions/test/whenUploadQueueAdded.test.ts
@@ -8,14 +8,12 @@ describe('whenUploadQueueAdded', () => {
 
   /**
    * Load sample data into the database before running the tests
-   * Allows a 5 minute timeout
+   * Allows a 1 minute timeout
    */
   beforeAll((done) => {
     (async () => {
       // Clear database
-      //console.log('preclearing the database')
       await deleteCollections(['catalog', 'sections', 'instructors', 'meta', 'groups'])
-      //console.log('database cleared')
 
       // Read data in from CSV file
       let rows: GradeDistributionCSVRow[] = [];
@@ -23,7 +21,6 @@ describe('whenUploadQueueAdded', () => {
         let value = GDR.tryFromRaw(record);
         if(value !== null) rows.push(value);
       }
-      //console.log('sample data loaded')
 
       // store a counter
       let totalRemovals = 0;
@@ -35,44 +32,33 @@ describe('whenUploadQueueAdded', () => {
         let numAdditions = snapshot.docChanges().filter(e => e.type === 'added').length;
         totalRemovals += numRemovals;
         totalAdditions += numAdditions;
-        //console.log(`snap! +${numAdditions} / -${numRemovals}`);
 
         // detect when upload has finished
         if(rows.length === totalRemovals && rows.length === totalAdditions) {
           // Prevents "A worker process has failed to exit gracefully and has been force exited."
-          //console.log('queue emptied')
           unsubscribe();
-          //console.log('unsubscribed')
           done();
-          //console.log('done\'d')
         }
       });
 
       // actually start upload in parallel
       await Promise.all(rows.map(e => db.collection('upload_queue').add(e)))
-      //console.log('queue filled')
     })();
   }, 1 * 60e3);
 
   afterAll(async () => {
     // Prevents "A worker process has failed to exit gracefully and has been force exited."
-    //console.log('whoops')
     await db.terminate();
     await firebase.delete();
-    //console.log('terminated?')
-    //return;
   })
 
   test('upload_queue is empty', async () => {
-    //console.log('foo')
     let docs = await db.collection('upload_queue').listDocuments();
     expect(docs.length).toEqual(0)
   })
 
   test('sections is not empty', async () => {
-    //console.log('bar')
     let docs = await db.collection('sections').listDocuments();
     expect(docs.length).toBeGreaterThan(0)
-    //expect(true).toEqual(true);
   })
 })
