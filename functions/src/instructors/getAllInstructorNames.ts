c5c7bba0759eaa3e7725e8ef3056a3b3a6925bd8
Austin Jackson <themacphage@gmail.com>
2021-07-18 23:21:22 -0500

    wrong name

--- functions/src/instructors/getAllInstructorNames.ts
+++ functions/src/instructors/getAllInstructorNames.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import { db } from '../_firebaseHelper';
 import { useCache, useCors } from '../_httpHelper';
 
-export const getAllCourseNames = functions.https.onRequest(async (request, response) => {
+export const getAllInstructorNames = functions.https.onRequest(async (request, response) => {
   useCors(response);
   useCache(response);
 
